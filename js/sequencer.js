637cd1028377537971cbf5e7a6c05026283f583d
Dan Bentley <danbentley9@gmail.com>
2013-04-17 16:35:57 +0100

    Removing old code

--- js/sequencer.js
+++ js/sequencer.js
@@ -30,7 +30,6 @@ define(['jquery'], function() {
 			this.play();
 			this.embedSounds();
 			this.addListeners();
-			this.drawSpaceInvader();
 		},
 		interval: null,
 		playling: true,
@@ -88,7 +87,7 @@ define(['jquery'], function() {
 		playAudioForRow: function(row) {
 			var index = this.getIndexForRow(row) - 1;
 			var audio = $('audio');
-			//audio[index].currentTime = 0;
+			audio[index].currentTime = 0;
 			audio[index].play();
 		},
 
@@ -130,16 +129,6 @@ define(['jquery'], function() {
 		updateActiveRow: function() {
 			this.currentRows = this.getCurrentRows();
 			this.currentRows.addClass('active');
-		},
-
-		drawSpaceInvader: function() {
-			var $spaceInvader = $('#grid-8-4, #grid-14-4, #grid-9-5, #grid-13-5, #grid-8-6, #grid-9-6, #grid-10-6, #grid-11-6, #grid-12-6, #grid-13-6, #grid-14-6, #grid-7-7, #grid-8-7, #grid-10-7, #grid-11-7, #grid-12-7, #grid-14-7, #grid-15-7, #grid-6-8, #grid-7-8, #grid-8-8, #grid-9-8, #grid-10-8, #grid-11-8, #grid-12-8, #grid-13-8, #grid-13-11, #grid-14-8, #grid-15-8, #grid-16-8, #grid-6-9, #grid-8-9, #grid-9-9, #grid-10-9, #grid-11-9, #grid-12-9, #grid-13-9, #grid-14-9, #grid-16-9, #grid-6-10, #grid-8-10, #grid-14-10, #grid-16-10, #grid-9-11, #grid-10-11, #grid-12-11, grid-13-11');
-
-			$spaceInvader = $('#grid-1-0, #grid-1-1, #grid-1-2, #grid-1-3, #grid-1-4, #grid-1-5, #grid-1-6, #grid-1-7, #grid-1-8');
-
-			$spaceInvader.each($.proxy(function(index, el) {
-//				this.selectCell($(el));
-			}, this));
 		},
 
 		play: function() {
